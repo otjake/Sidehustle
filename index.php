a79bb6b0a92d74eb4ae62aa05b03ca54de9cd632
jake <41896862+otjake@users.noreply.github.com>
2021-02-22 13:47:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
 <?php
-$prefix="0";
+//$prefix="0";
 //200 numbers
-for ($j =1; $j<=200; $j++) {
+// for ($j =1; $j<=200; $j++) {
 
-    //getting random numbers 12 digits long
-    $input= rand(0,pow(10,12)-1);
+//     //getting random numbers 12 digits long
+//     $input= rand(0,pow(10,12)-1);
 
-    //counting number lenght
-    $count = $input !== 0 ? floor(log10($input) + 1) : 1;
+//     //counting number lenght
+//     $count = $input !== 0 ? floor(log10($input) + 1) : 1;
 
-    //if its not up to 12 add a 0 $prefix, some recharge cards start with 0
+//     //if its not up to 12 add a 0 $prefix, some recharge cards start with 0
 
-    for ($i = $count + 1; $i <= 12; $i++) {
+//     for ($i = $count + 1; $i <= 12; $i++) {
 
-        $input = $prefix . $input;
-    }
-    //output
-    echo "(".$j.") ". $input;
-    echo "<hr>";
-    echo "<br>";
+//         $input = $prefix . $input;
+//     }
+//     //output
+//     echo "(".$j.") ". $input;
+//     echo "<hr>";
+//     echo "<br>";
 
+// }
+
+for ($i=0; $i <= 200 ; $i++) { 
+    $randArr[] = rand(pow(10,15), pow(10,16)-1) . "<br>"; // This generates random 16 digits random numbers
 }
+print_r($randArr);
 ?>
